be8b064bcf610047c16849548ed0065e93d38151
Avik Das <avik.das@berkeley.edu>
2015-12-10 18:06:49 -0800

    Implement short-circuiting "or" operator

--- test/success/boolean-operator.scm
+++ test/success/boolean-operator.scm
@@ -31,3 +31,27 @@
 
 (newline)
 (newline)
+
+;; AND ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; booleans
+(display (or #t)) (newline)
+(display (or #t #t)) (newline)
+(display (or #t #f)) (newline)
+(display (or #f #f)) (newline)
+(display (or #t #t #t)) (newline)
+(display (or #f #f #f)) (newline)
+
+(newline)
+
+; truthy vs. falsey values
+(display (or '() #f)) (newline)
+(display (or #f #f)) (newline)
+(display (or '(1) #f)) (newline)
+(display (or 1 #f)) (newline)
+
+(newline)
+
+; short circuiting
+(display (or (print-and-true) (print-and-false))) (newline)
+(display (or (print-and-false) (print-and-true))) (newline)
