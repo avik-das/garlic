18657cd930df5f47333477c12b1fe1a4704c34d0
Avik Das <avik.das@berkeley.edu>
2017-12-09 17:50:16 -0800

    Add `string:` stdlib functions needed for lexer
    - `string:null?`       - for checking if a string is empty
    - `string:at`          - get a character at a given index
    - `string:string-tail` - get the tail end of a string beginning at a
                             given index
    
    These will allw be useful for the recursive lexer.

--- stdlib-includes/string.c
+++ stdlib-includes/string.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static garlic_value_t nullp(garlic_value_t str) {
+    if (garlic_get_type(str) != GARLIC_TYPE_STRING) {
+        error_and_exit("ERROR - string:nullp - value is not a string");
+    }
+
+    return garlic_string_length(str) == 0 ?  TRUE_VALUE : FALSE_VALUE;
+}
+
 garlic_value_t concat(garlic_value_t args) {
     if (args == NIL_VALUE) {
         return garlic_empty_string;
@@ -50,6 +58,28 @@ garlic_value_t concat_list(garlic_value_t list) {
     return concat(list);
 }
 
+garlic_value_t string_tail(garlic_value_t str, garlic_value_t index) {
+    if (garlic_get_type(str) != GARLIC_TYPE_STRING) {
+        error_and_exit("ERROR - string:string-tail - value is not a string");
+    }
+
+    if (garlic_get_type(index) != GARLIC_TYPE_FIXNUM) {
+        error_and_exit("ERROR - string:string-tail - index is not an integer");
+    }
+
+    int64_t i = garlicval_to_int(index);
+
+    if (i == 0) {
+        return str;
+    }
+
+    if (i >= garlic_string_length(str)) {
+        return garlic_empty_string;
+    }
+
+    return garlic_wrap_string(garlic_unwrap_string(str) + i);
+}
+
 garlic_value_t symbol_to_str(garlic_value_t sym) {
     if (garlic_get_type(sym) != GARLIC_TYPE_ATOM) {
         printf("non-symbol passed to symbol->str\n");
@@ -70,10 +100,33 @@ garlic_value_t string_equalp(garlic_value_t str1, garlic_value_t str2) {
     return strcmp(cstr1, cstr2) == 0 ? TRUE_VALUE : FALSE_VALUE;
 }
 
+garlic_value_t character_at(garlic_value_t str, garlic_value_t index) {
+    if (garlic_get_type(str) != GARLIC_TYPE_STRING) {
+        error_and_exit("ERROR - string:at - value is not a string");
+    }
+
+    if (garlic_get_type(index) != GARLIC_TYPE_FIXNUM) {
+        error_and_exit("ERROR - string:at - index is not an integer");
+    }
+
+    int64_t i = garlicval_to_int(index);
+    if (i >= garlic_string_length(str)) {
+        return NIL_VALUE;
+    }
+
+    char *result = (char *) malloc(sizeof(char) * 2);
+    result[0] = garlic_unwrap_string(str)[i];
+    result[1] = 0;
+    return garlic_wrap_string(result);
+}
+
 garlic_native_export_t string_exports[] = {
+    {"null?", nullp, 1},
     {"concat", concat, 0, 1},
     {"concat-list", concat_list, 1},
+    {"string-tail", string_tail, 2},
     {"symbol->str", symbol_to_str, 1},
     {"string=?", string_equalp, 2},
+    {"at", character_at, 2},
     0
 };
--- test/success/string-lib.scm
+++ test/success/string-lib.scm
@@ -22,3 +22,24 @@
 
 (display (str:string=? "abc" "abc")) (newline)
 (display (str:string=? "ABC" "abc")) (newline)
+
+(newline)
+
+(display (str:at "abcd" 0)) (newline)
+(display (str:at "abcd" 1)) (newline)
+(display (str:at "abcd" 2)) (newline)
+(display (str:at "abcd" 3)) (newline)
+(display (str:at "abcd" 4)) (newline)
+
+(newline)
+
+(display (str:string-tail "abcd" 0)) (newline)
+(display (str:string-tail "abcd" 1)) (newline)
+(display (str:string-tail "abcd" 2)) (newline)
+(display (str:string-tail "abcd" 3)) (newline)
+(display (str:string-tail "abcd" 4)) (newline)
+
+(newline)
+
+(display (str:null? "not-empty")) (newline)
+(display (str:null? "")) (newline)
