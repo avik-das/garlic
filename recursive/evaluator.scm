6e03f73f38e912a610639161e8e6288e679a0e35
Avik Das <avik.das@berkeley.edu>
2017-12-14 23:07:12 -0800

    Separate out evaluator in recursive implementation
    While I'd like to make the recursive implementation a proper compiler,
    I'll start by focusing on the AST transformations first. In order to do
    so, I need some way to show that the AST is valid.

---
+++ recursive/evaluator.scm
@@ -0,0 +1,68 @@
+;; An implementation of an interpreter for an AST with the interfaces provided
+;; by the "ast" module. This AST is assumed to have been produced from some
+;; input source code, but the details of that are irrelevant. The evaluator
+;; simply executes the code represented by the AST.
+
+(require string => str)
+
+(require "ast")
+
+;; FRAME ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-frame parent . symbols)
+  (cons parent symbols))
+
+(define frame-parent car)
+(define frame-symbols cdr)
+
+(define root-frame
+  (make-frame '()
+              (cons "+" +)
+              (cons "-" -)
+              (cons "*" *)
+              (cons "display" display)
+              (cons "newline" newline)) )
+
+(define (find-in-frame frame symbol)
+  (define (find-in-list ls)
+    (find
+      (lambda (entry) (str:string=? (car entry) symbol))
+      ls))
+
+  (if (null? frame)
+    '()
+    (let* ((parent (frame-parent frame))
+           (symbols (frame-symbols frame))
+           (entry (find-in-list symbols)))
+      (if entry
+        (cdr entry)
+        (find-in-frame parent symbol)) )) )
+
+;; EVALUATION ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (eval-call-function call frame)
+  (let ((fn (ast:function-call-get-function call))
+        (args (ast:function-call-get-args call)))
+    ; In the future, instead of calling `apply`, a new frame will be
+    ; constructed, and the body of the function will be evaluated with respect
+    ; to that frame.
+    (apply
+      (recursive-eval fn frame)
+      (map (recursive-eval-with-frame frame) args)) ))
+
+(define (recursive-eval tree frame)
+  (cond ((ast:var? tree) (find-in-frame frame (ast:var-get-name tree)))
+        ((ast:int? tree) (ast:int-get-value tree))
+        ((ast:function-call? tree) (eval-call-function tree frame)) ))
+
+(define (recursive-eval-with-frame frame)
+  (lambda (tree)
+    (recursive-eval tree frame)) )
+
+(define (eval-module module)
+  (foreach
+    (recursive-eval-with-frame root-frame)
+    (ast:module-get-statements module) ))
+
+(module-export
+  eval-module)
--- recursive/garlic.scm
+++ recursive/garlic.scm
@@ -4,34 +4,9 @@
 (require "lexer")
 (require "parser")
 (require "ast")
+(require "evaluator")
 
-;; TEST EVALUATOR (TEMPORARY) ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Just to make sure the lexer and parser work, this extremely simple evaluator
-;; "runs" the program represented by the AST produced by the parser.
-
-(define (eval-lookup-var name)
-  (cond ((str:string=? name "+") +)
-        ((str:string=? name "-") -)
-        ((str:string=? name "*") *)
-        ((str:string=? name "display") display)
-        ((str:string=? name "newline") newline) ))
-
-(define (eval-call-function call)
-  (let ((fn (ast:function-call-get-function call))
-        (args (ast:function-call-get-args call)))
-    (apply
-      (recursive-eval fn)
-      (map recursive-eval args)) ))
-
-(define (recursive-eval tree)
-  (cond ((ast:module? tree)
-         (foreach recursive-eval (ast:module-get-statements tree)))
-        ((ast:var? tree) (eval-lookup-var (ast:var-get-name tree)))
-        ((ast:int? tree) (ast:int-get-value tree))
-        ((ast:function-call? tree) (eval-call-function tree)) ))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (if (not (= (length *argv*) 2))
   (begin
@@ -42,4 +17,4 @@
 (define input (file:read-text (car (cdr *argv*))) )
 (define lexed (lexer:lex input))
 (define parsed (parser:parse lexed))
-(recursive-eval parsed)
+(evaluator:eval-module parsed)
